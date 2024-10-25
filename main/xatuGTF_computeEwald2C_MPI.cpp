6582a25135ab0cec19bad493eb3beb22dffab2f1
GarciaBlazquez <159708537+GarciaBlazquez@users.noreply.github.com>
2024-10-25 16:48:30 +0200

    Update xatuGTF_computeEwald2C_MPI.cpp

--- main/xatuGTF_computeEwald2C_MPI.cpp
+++ main/xatuGTF_computeEwald2C_MPI.cpp
@@ -19,14 +19,14 @@ int main(int argc, char* argv[]){
     }
 
     // INPUT PARAMETERS /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    std::string outp_file = "InputFiles/hBN_HSE06.outp";
-    int ncells = 179;                                 //Number of H(R) and S(R) matrices taken into account from the .outp file
-    std::string bases_file = "InputFiles/Bases_NEW_def2-TZVPPD-RIFIT.txt";
-    std::string savefile = "def2-TZVPP-RIFIT";        //Result will be saved in file Results/1-Integrals/E2Mat_savefile.E2c
+    std::string outp_file = "InputFiles/Pho_PBE0_1D_0f.outp";
+    int ncells = 43;                                 //Number of H(R) and S(R) matrices taken into account from the .outp file
+    std::string bases_file = "InputFiles/Bases_custom.txt";
+    std::string savefile = "custom";        //Result will be saved in file Results/1-Integrals/E2Mat_savefile.E2c
     bool is_for_Dk = false;                           //True for scalei_supercell corresponding to Deltak. If true, the integrals and lattice vectores will be stored with the extension .E2cDk instead of the usual .E2c 
-    std::vector<int32_t> scalei_supercell = {72,72};  //Scaling factor for the corresponding original (unit cell) Bravais basis vectors Ri to form the supercell
-    int nR = 40;                                      //Minimum number of external supercell lattice vectors to be included in the Ewald direct lattice sum
-    int nG = 200;                                     //Minimum number of reciprocal supercell vectors to be included in the Ewald reciprocal lattice sum
+    std::vector<int32_t> scalei_supercell = {88,66};  //Scaling factor for the corresponding original (unit cell) Bravais basis vectors Ri to form the supercell
+    int nR = 60;                                      //Minimum number of external supercell lattice vectors to be included in the Ewald direct lattice sum
+    int nG = 300;                                     //Minimum number of reciprocal supercell vectors to be included in the Ewald reciprocal lattice sum
     int tol = 8;                                      //Threshold tolerance for the integrals: only entries > 10^-tol are stored
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
