8615b99dcc8937be5c52e756e2f86c4c36295bec
GarciaBlazquez <159708537+GarciaBlazquez@users.noreply.github.com>
2024-10-25 15:04:11 +0200

    Create xatuGTF_computeDipoles_MPI.cpp

---
+++ main/xatuGTF_computeDipoles_MPI.cpp
@@ -0,0 +1,39 @@
+#include <mpi.h>
+#include "xatu.hpp"
+
+int main(int argc, char* argv[]){
+
+    int procMPI_rank, procMPI_size;
+
+    MPI_Init (&argc,&argv);
+    MPI_Comm_rank (MPI_COMM_WORLD, &procMPI_rank);
+    MPI_Comm_size (MPI_COMM_WORLD, &procMPI_size);
+    if(procMPI_rank == 0){
+        xatu::printHeaderGTFprovisional();
+        std::cout << std::endl;
+        xatu::printParallelizationMPI(procMPI_size);
+        std::cout << std::endl;
+        std::cout << "+---------------------------------------------------------------------------+" << std::endl;
+        std::cout << "|                          DIPOLE (X,Y,Z) INTEGRALS                         |" << std::endl;
+        std::cout << "+---------------------------------------------------------------------------+" << std::endl;
+    }
+
+    // INPUT PARAMETERS /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::string outp_file = "InputFiles/hBN_HSE06.outp";
+    int ncells = 179;                                 //Number of H(R) and S(R) matrices taken into account from the .outp file
+    std::string bases_file = "InputFiles/Bases_NEW_def2-TZVPPD-RIFIT.txt";
+    std::string savefile = "def2-TZVPP-RIFIT";        //Result will be saved in file Results/1-Integrals/dipoleMat_savefile.dip
+    int nR = 200;                                     //Minimum number of direct lattice vectors for which the dipole integrals will be computed
+    int tol = 8;                                      //Threshold tolerance for the integrals: only entries > 10^-tol are stored
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    xatu::ConfigurationCRYSTAL_MPI CRYSTALconfig(outp_file, procMPI_rank, procMPI_size, ncells, true);
+    xatu::ConfigurationGTF_MPI GTFconfig(procMPI_rank, procMPI_size, CRYSTALconfig.nspecies, CRYSTALconfig.atomic_number_ordering, bases_file);
+    xatu::IntegralsBase IntBase(CRYSTALconfig, GTFconfig); 
+
+    xatu::IntegralsDipole_MPI Dipole(IntBase, procMPI_rank, procMPI_size, tol, nR, savefile); 
+
+    MPI_Finalize();
+    return 0;
+
+}
